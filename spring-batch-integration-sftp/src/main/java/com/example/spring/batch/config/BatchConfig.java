194d65b2d43e5f4431345b4748ba6db499b1adc7
Fernando Bartolome <138666840+fernandobartolome02@users.noreply.github.com>
2023-08-17 12:46:17 +0200

    commiteo

--- spring-batch-integration-sftp/src/main/java/com/example/spring/batch/config/BatchConfig.java
+++ spring-batch-integration-sftp/src/main/java/com/example/spring/batch/config/BatchConfig.java
@@ -6,6 +6,7 @@ import com.example.spring.domain.model.Contract;
 import com.example.spring.domain.model.ContractProcessed;
 import com.example.spring.domain.repository.ContractProcessedRepository;
 import com.example.spring.domain.repository.ContractRepository;
+import jakarta.annotation.Resource;
 import lombok.AllArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -17,14 +18,23 @@ import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.data.domain.Sort;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.Locale;
+
+import static java.lang.Integer.parseInt;
 
 @Configuration
 @AllArgsConstructor
@@ -57,9 +67,22 @@ public class BatchConfig {
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager(){
-        return new ResourcelessTransactionManager();
+    public FlatFileItemWriter<ContractProcessed> writerCSV() {
+        FlatFileItemWriter<ContractProcessed> writer = new FlatFileItemWriter<>();
+        writer.setResource(new FileSystemResource("src/main/resources/output/output.csv"));
+        writer.setLineAggregator(new DelimitedLineAggregator<ContractProcessed>() {
+            {
+                setDelimiter(",");
+                setFieldExtractor(new BeanWrapperFieldExtractor<ContractProcessed>() {
+                    {
+                        setNames(new String[] { "policyId", "policy", "policySituation", "policyBrand", "policyDate", "expired", "disabled"});
+                    }
+                });
+            }
+        });
+        return writer;
     }
+
 
     @Bean
     public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager, CustomChunkListener customChunkListener) {
@@ -67,17 +90,29 @@ public class BatchConfig {
                 .<Contract, ContractProcessed>chunk(5, transactionManager)
                 .listener(customChunkListener)
                 .reader(reader())
-                .processor(contract -> new ContractProcessed(contract.getPolicyId(), contract.getPolicy(),  contract.getPolicySituation(), contract.getPolicyBrand(), contract.getPolicyDate(), contract.isExpired(), contract.isDisabled()))
+                .processor(contract -> new ContractProcessed(contract.getPolicyId(), contract.getPolicy(),  contract.getPolicySituation().toUpperCase(), contract.getPolicyBrand().toUpperCase(), LocalDateTime.now(), contract.isExpired(), contract.isDisabled()))
                 .writer(writer())
                 .build();
     }
 
     @Bean
-    public Job convertCarToUppercaseJob(Step step1, JobRepository jobRepository , JobListener listener) {
-        return new JobBuilder("convertCarToUppercaseJob", jobRepository)
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager transactionManager, CustomChunkListener customChunkListener) {
+        return new StepBuilder("step2", jobRepository)
+                .<Contract, ContractProcessed>chunk(5, transactionManager)
+                .listener(customChunkListener)
+                .reader(reader())
+                .processor(contract -> new ContractProcessed(contract.getPolicyId(), contract.getPolicy() + "/Procesada",  contract.getPolicySituation(), contract.getPolicyBrand(), contract.getPolicyDate(), contract.isExpired(), contract.isDisabled()))
+                .writer(writerCSV())
+                .build();
+    }
+
+    @Bean
+    public Job passContractToContractProcessed(Step step1, Step step2, JobRepository jobRepository , JobListener listener) {
+        return new JobBuilder("passContractToContractProcessed", jobRepository)
                 .incrementer(new RunIdIncrementer())
                 .listener(listener)
                 .start(step1)
+                .next(step2)
                 .build();
     }
 
