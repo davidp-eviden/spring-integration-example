ebdcd0a13f7317e8c619b8be34d84e75bd4c5ddc
David Poma <david.poma@atos.net>
2023-08-21 13:17:35 +0200

    Fix typo

--- spring-batch-integration-sftp/README.md
+++ spring-batch-integration-sftp/README.md
@@ -1,16 +1,19 @@
 ## Description
 
-An Example of Read from a database , write in files and send to Sftp client using Spring Batch and Spring integration.
+An Example of Read from a database , write in files and send to Sftp server using Spring Batch and Spring integration.
 
 ## Getting started
 
+
+1. Run the docker services
 ```shell
 cd docker
 docker compose up -d
 ```
 
 
+2. Run the application with maven
 
-
-
-
+```shell
+mvn spring-boot:run
+```
--- spring-batch-integration-sftp/src/main/java/com/example/spring/integration/config/IntegrationConfig.java
+++ spring-batch-integration-sftp/src/main/java/com/example/spring/integration/config/IntegrationConfig.java
@@ -3,35 +3,22 @@ package com.example.spring.integration.config;
 import org.apache.sshd.sftp.client.SftpClient;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.ResolvableType;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.Resource;
 import org.springframework.expression.common.LiteralExpression;
 import org.springframework.http.HttpMethod;
-import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.file.remote.session.CachingSessionFactory;
 import org.springframework.integration.file.remote.session.SessionFactory;
 import org.springframework.integration.http.dsl.Http;
-import org.springframework.integration.http.inbound.HttpRequestHandlingMessagingGateway;
-import org.springframework.integration.http.inbound.RequestMapping;
 import org.springframework.integration.sftp.outbound.SftpMessageHandler;
 import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.SubscribableChannel;
 
-import java.util.Collections;
 import java.util.Date;
 
 @Configuration
@@ -54,11 +41,13 @@ public class IntegrationConfig {
             JobExecution jobExecution  = this.jobLauncher.run(this.job, jobParametersBuilder.toJobParameters());
 
             // Si el trabajo se ejecuta correctamente, se devuelve una respuesta exitosa.
-            return MessageBuilder.withPayload(String.format("The job was launched sucessfully with the following details: \n %s", jobExecution.toString())).build();
+            return MessageBuilder
+                    .withPayload(String.format("The job was launched successfully with the following details: \n %s", jobExecution.toString()))
+                    .build();
         } catch (Exception e) {
             // Si se produce una excepción al ejecutar el trabajo, manejarla aquí.
             // Puedes devolver un mensaje de error o cualquier respuesta apropiada.
-            return MessageBuilder.withPayload("Error launching job: " + e.getMessage()).build();
+            return MessageBuilder.withPayload("Error launching the job with the following error: " + e.getMessage()).build();
         }
     }
 
@@ -66,30 +55,20 @@ public class IntegrationConfig {
     public IntegrationFlow inbound() {
         return IntegrationFlow.from(Http.inboundGateway("/launch")
                         .requestMapping(m -> m.methods(HttpMethod.POST))
-                        .replyTimeout(300) // The program only has 300 ms to reply.
-                )
-                .channel("launchJobsChannel")
+                        .replyTimeout(300)) // The program only has 300 ms to reply.
+                .channel("launchJobsChannel") // Subscription to the launchJobsChannel
                 .get();
     }
 
 
-    //TODO: Manage the response
     @Bean
     public IntegrationFlow outbound(){
+        //TODO: Manage the response
         return IntegrationFlow.from("launchJobsChannel")
                 .handle(Http.outboundGateway("/launch")
                         .httpMethod(HttpMethod.POST))
                 .get();
     }
-
-
-    /*
-        MessageChannel replyChannel = (MessageChannel) message.getHeaders().getReplyChannel();
-        MessageBuilder.fromMessage(message);
-        Message<String> newMessage = MessageBuilder
-                .withPayload("Welcome " + message.getPayload() + " to Integration").build();
-            replyChannel.send(message);
-    */
 
     @Bean
     public SessionFactory<SftpClient.DirEntry> sftpSessionFactory(SftpConfig config){
